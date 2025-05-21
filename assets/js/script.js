786cc7ed238f340a1d14b78e8aa4e4af91d30577
Muthommi <kelvinmartin16@gmail.com>
2025-05-21 19:12:01 +0300

    Fixed my javascript file

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,8 +9,16 @@ if (searchBox) {
         let projects = document.querySelectorAll('.project-card');
 
         projects.forEach(project => {
-            let title = project.getAttribute('data-title').toLowerCase();
-            project.style.display = title.includes(filter) ? "block" : "none";
+            let title = project.getAttribute('data-title')?.toLowerCase() || "";
+	    if (title.includes(filter)) {
+	        project.style.opacity = "1";
+	        project.style.transform = "scale(1)";
+	        project.style.display = "block";
+	    } else {
+	        project.style.opacity = "0";
+	        project.style.transform = "scale(0.95)";
+	        setTimeout(() => project.style.display = "none", 300);
+	    }
         });
     });
 }
