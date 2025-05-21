2f20defa2d926eb83c7bcc217d3fc464c315a107
Muthommi <kelvinmartin16@gmail.com>
2025-05-21 18:37:21 +0300

    Improving my portfolio

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,12 +2,15 @@ document.getElementById('theme-toggle').addEventListener('click', function () {
     document.body.classList.toggle('dark-mode');
 });
 
-document.getElementById('search').addEventListener('keyup', function() {
-    let filter = this.value.toLowerCase();
-    let projects = document.querySelectorAll('.project-card');
+let searchBox = document.getElementById('search');
+if (searchBox) {
+    searchBox.addEventListener('keyup', function() {
+        let filter = this.value.toLowerCase();
+        let projects = document.querySelectorAll('.project-card');
 
-    projects.forEach(project => {
-        let title = project.getAttribute('data-title').toLowerCase();
-        project.style.display = title.includes(filter) ? "block" : "none";
+        projects.forEach(project => {
+            let title = project.getAttribute('data-title').toLowerCase();
+            project.style.display = title.includes(filter) ? "block" : "none";
+        });
     });
-});
+}
